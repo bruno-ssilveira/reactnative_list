84a7bd9484c42bc370839834e8f06177ed7aa7a6
bruno <bruno.santossilveira001@gmail.com>
2025-01-06 07:25:28 -0300

    The 'New Tasks' field was repositioned, and cards with checkboxes for the tasks were added.

--- app/index.tsx
+++ app/index.tsx
@@ -2,20 +2,63 @@ import tarefasMock from '@/data/constants/tarefas';
 import Tarefa from "@/data/model/Tarefa";
 import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
 import { useState } from "react";
-import {botao, input, mb10, flexRow, flex1, gapX5, gapY5, textWhite, bgRed500, bgBlue500, bgGreen500, alignCenter, px2, textZinc500, lineThrough, w9_10, fontBold, border} from "@/styles";
+import {checkBox, taskCard, botao, deleteStyle, pressedScale, input, mb10, mt10, ml10, marginTask, flexRow, flex1, gapX5, gapY5, textWhite, bgRed500, bgBlue500, bgGreen500, bgYellow500, bgLightBlue500, alignCenter, px2, textZinc500, lineThrough, w9_10, fontBold} from "@/styles";
 import { AntDesign } from '@expo/vector-icons';
 
 export default function Index() {
   const [descricao, setDescricao] = useState('')
   const [tarefas, setTarefas] = useState<Tarefa[]>(tarefasMock)
+  const [editandoId, setEditandoId] = useState<number | null>(null)
+  const [descricaoTemp, setDescricaoTemp] = useState('')
 
   function excluirTarefa(tarefa: Tarefa) {
     const novasTarefas = tarefas.filter((t) => t.id != tarefa.id)
     setTarefas(novasTarefas)
   }
 
-  function adicionarTarefa(tarefa: Tarefa) {
-    const novasTarefas = tarefas.push()
+  function adicionarTarefa() {
+    if(descricao.trim() === '') {
+      return
+    }
+    const novaTarefa: Tarefa = {
+      id: tarefas.length +1,
+      descricao,
+      concluida: false,
+    }
+    setTarefas([...tarefas, novaTarefa]);
+    setDescricao('');
+  }
+
+  function editarTarefa(tarefa: Tarefa) {
+    if(editandoId === tarefa.id) {
+      const novasTarefas = tarefas.map((t) => {
+        if(t.id === tarefa.id) {
+          return { ...t, descricao: descricaoTemp };
+        }
+        return t
+      });
+      setTarefas(novasTarefas);
+      setEditandoId(null);
+      setDescricaoTemp('');
+    } else {
+      // Entrar no modo de edição
+      setEditandoId(tarefa.id);
+      setDescricaoTemp(tarefa.descricao);
+    }
+  }
+
+  function salvarEdicao() {
+    if (editandoId !== null) {
+      const novasTarefas = tarefas.map((t) => {
+        if (t.id === editandoId) {
+          return { ...t, descricao: descricaoTemp };
+        }
+        return t;
+      });
+      setTarefas(novasTarefas);
+      setEditandoId(null);
+      setDescricaoTemp('');
+    }
   }
 
   function alternarConclusao(tarefa: Tarefa) {
@@ -32,33 +75,40 @@ export default function Index() {
     <ScrollView
       contentContainerStyle={{
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "space-between",
         alignItems: "center",
       }}
     >
-      <View style={[flexRow, gapX5, w9_10, mb10]}>
-        <TextInput value={descricao} onChangeText={setDescricao} style={[input, flex1]} />
-        <Pressable style={[botao, bgGreen500, px2]} onPress={() => excluirTarefa(tarefa)}>
-          <AntDesign name='plus' size={16} color="white" />
-        </Pressable>
-      </View>
 
-      <View style={[w9_10, gapY5]}>
+      {/* -------- Lista de Tarefas -------- */}
+      <View style={[w9_10, gapY5, mt10]}>
         {tarefas.map((tarefa) => (
-          <View key={tarefa.id} style={[flexRow, gapX5, alignCenter]}>
-            <View style={[flex1]}>
-              <Text style={tarefa.concluida ? [textZinc500, lineThrough] : []}>
-                {tarefa.descricao}
-              </Text>
-            </View>
-            <Pressable style={tarefa.concluida ? [botao, bgGreen500, px2] : [botao, bgBlue500, px2]} onPress={() => alternarConclusao(tarefa)}>
-              <AntDesign name={'check'} size={16} color="white" />
-            </Pressable>
-            <Pressable style={[botao, bgRed500, px2]} onPress={() => excluirTarefa(tarefa)}>
-              <AntDesign name='delete' size={16} color="white" />
-            </Pressable>
-          </View>
+          <Pressable key={tarefa.id} style={({pressed}) => [flexRow, gapX5, alignCenter, pressed ? bgLightBlue500 : null]} onLongPress={() => editarTarefa(tarefa)} onBlur={salvarEdicao}>
 
+            {/* Descrição da Tarefa */}
+            <View style={[flexRow, taskCard]}>
+
+              {/* Status Tarefa */}
+              <Pressable style={({pressed}) => [tarefa.concluida ? [checkBox, px2, alignCenter] : [checkBox, px2, alignCenter], pressed ? pressedScale : null]} onPress={() => alternarConclusao(tarefa)}>
+                <Text>{tarefa.concluida ? '✔' : ''}</Text>
+              </Pressable>
+              
+              <View style={[flex1, marginTask]}>
+                {
+                  editandoId === tarefa.id ? (<TextInput value={descricaoTemp} onChangeText={setDescricaoTemp} onBlur={salvarEdicao} style={[input, fontBold]} />)
+                  : <Text style={tarefa.concluida ? [textZinc500, lineThrough] : []}> {tarefa.descricao} </Text>
+                }
+              </View>
+
+              {/* Excluir Tarefa */}
+              <Pressable style={({pressed}) => [deleteStyle, pressed ? pressedScale : null]} onPress={() => excluirTarefa(tarefa)}>
+                <Text>X</Text>
+              </Pressable>
+
+            </View>
+            
+
+          </Pressable>
         ))}
 
 
@@ -68,6 +118,14 @@ export default function Index() {
         </Link>
         <Text>Edit app/index.tsx to edit this screen.</Text>
         */}
+      </View>
+
+      {/* -------- Adicionar Tarefa -------- */}
+      <View style={[flexRow, gapX5, w9_10, mb10]}>
+        <TextInput placeholder='Adicionar nova tarefa...' value={descricao} onChangeText={setDescricao} style={[input, flex1]} />
+        <Pressable style={({pressed}) => [botao, bgBlue500, px2, pressed ? bgGreen500 : bgBlue500, pressed ? pressedScale : null]} onPress={adicionarTarefa}>
+          <AntDesign name='plus' size={16} color="white" />
+        </Pressable>
       </View>
 
     </ScrollView>
--- styles/utility.ts
+++ styles/utility.ts
@@ -25,6 +25,13 @@ const style = StyleSheet.create({
     bgRed500: {
         backgroundColor: '#EF4444',
     },
+    bgYellow500: {
+        backgroundColor: '#F59E0B',
+    },
+    bgLightBlue500: {
+        backgroundColor: '#B3DFFC',
+        borderRadius: 10,
+    },
     px2: {
         paddingHorizontal: 8,
     },
@@ -55,6 +62,46 @@ const style = StyleSheet.create({
     mb10: {
         marginBottom: 40,
     },
+    mt10: {
+        marginTop: 40,
+    },
+    ml10: {
+        marginLeft: 40,
+    },
+    marginTask: {
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    pressedScale: {
+        transform: [{scale: 0.90}], // Reduz 10% do tamanho
+    },
+    taskCard: {
+        borderColor: '#444444',
+        borderWidth: 2,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingHorizontal: 10,
+        borderRadius: 10,
+    },
+    checkBox: {
+        width: 34,
+        height: 34,
+        borderRadius: 4,
+        borderWidth: 2,
+        borderColor: '#718096',
+        position: 'relative',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    deleteStyle: {
+        backgroundColor: '#EF4444',
+        width: 24,
+        height: 24,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
+        borderRadius: 10,
+    }
 })
 
 export const flexRow = style.flexRow
@@ -65,6 +112,8 @@ export const roundedMd = style.roundedMd
 export const bgGreen500 = style.bgGreen500
 export const bgBlue500 = style.bgBlue500
 export const bgRed500 = style.bgRed500
+export const bgYellow500 = style.bgYellow500
+export const bgLightBlue500 = style.bgLightBlue500
 export const px2 = style.px2
 export const px4 = style.px4
 export const py2 = style.py2
@@ -75,3 +124,10 @@ export const textZinc500 = style.textZinc500
 export const w9_10 = style.w9_10
 export const fontBold = style.fontBold
 export const mb10 = style.mb10
+export const mt10 = style.mt10
+export const ml10 = style.ml10
+export const marginTask = style.marginTask
+export const pressedScale = style.pressedScale
+export const taskCard = style.taskCard
+export const checkBox = style.checkBox
+export const deleteStyle = style.deleteStyle
